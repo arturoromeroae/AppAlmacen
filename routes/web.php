afc8a15ffb6610e7b43576f66d9c9ffeb702fca9
Arturo Romero <aromero@aeonlinesolutions.com>
2021-03-05 23:21:47 -0500

    se agregan las rutas

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/almacen', [App\Http\Controllers\StockController::class, 'stock'])->name('almacen');
+
+Route::any('/almacen/{id}', [App\Http\Controllers\StockController::class, 'put_stock'])->name('stock.edit');
+
+Route::get('/mantenimiento', [App\Http\Controllers\MaintanceController::class, 'index'])->name('mantenimiento');
+
+Route::post('/mantenimiento', [App\Http\Controllers\MaintanceController::class, 'sender'])->name('mantenimiento.add');
+
+Route::any('/mantenimiento/{id}', [App\Http\Controllers\MaintanceController::class, 'put_product'])->name('mantenimiento.edit');
+
+Route::get('/ventas', [App\Http\Controllers\SalesController::class, 'index'])->name('ventas');
+
+Route::get('/repuestos', [App\Http\Controllers\SpareController::class, 'index'])->name('repuestos');
