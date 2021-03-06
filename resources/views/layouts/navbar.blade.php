db6a85a839428a21c17ffdabdf8311f4e85a9f42
Arturo Romero <aromero@aeonlinesolutions.com>
2021-03-05 23:39:49 -0500

    se modifica el home

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -24,7 +24,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-lg bg-primary">
             <div class="container-fluid">
-                <a id="logo-navbar" class="navbar-brand text-navbar hover-navbar" href="#">Logo</a>
+                <a id="logo-navbar" class="navbar-brand text-navbar hover-navbar" href="#">Almacen</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
